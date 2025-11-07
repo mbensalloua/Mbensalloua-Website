5bd1586e4f199f19fe9a1c63dd052daadcd2f780
mbensalloua <100726587+mbensalloua@users.noreply.github.com>
2025-11-06 18:27:06 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import About from "./pages/About";
@@ -8,16 +8,14 @@ import "./styles/globals.css";
 
 export default function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen bg-background text-foreground">
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className="flex flex-col min-h-screen bg-background text-foreground">
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
